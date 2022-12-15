4b323d2118eeae6b5bab6b2a69773064bd5b8cad
Flu3nt <gnedyalkov94@gmail.com>
2022-12-15 14:20:53 +0200

    Create a basic rock, paper, scissors game against the computer

---
+++ rock_paper_scissors.py
@@ -0,0 +1,34 @@
+import random
+
+print("Hello, let's play rock paper scissors. Make your choice")
+
+player_choice = input().lower()
+computer_choice = random.choice(["rock", "paper", "scissors"])
+
+if player_choice == "rock":
+    if computer_choice == "paper":
+        print("Computer wins this one")
+    elif computer_choice == "scissors":
+        print("Player wins. Fudge technology!")
+    elif computer_choice == "rock":
+        print("This round is equal. Well played try again!")
+elif player_choice == "paper":
+    if computer_choice == "scissors":
+        print("Computer wins this one")
+    elif computer_choice == "rock":
+        print("Player wins. Fudge technology!")
+    elif computer_choice == "paper":
+        print("This round is equal. Well played try again!")
+elif player_choice == "scissors":
+    if computer_choice == "rock":
+        print("Computer wins this one")
+    elif computer_choice == "paper":
+        print("Player wins. Fudge technology!")
+    elif computer_choice == "scissors":
+        print("This round is equal. Well played try again!")
+else:
+    player_choice = input().lower()
+
+
+print("Computer chose: " + computer_choice)
+print("Player chose: " + player_choice)
